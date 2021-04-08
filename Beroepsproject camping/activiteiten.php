c41b7395bf369570edfff91202c8c951839ef96b
s-bhageloe <2117940@talnet.nl>
2021-04-09 00:33:32 +0200

    camping village bestanden

---
+++ Beroepsproject camping/activiteiten.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+	<title>Home</title>
+</head>
+<body>
+  <div class="background">
+    <div class="content">
+      <!-- Links -->
+      <header>
+        <div class="header">
+          <nav class="navigation">
+            <a href="index.php" class="navbar-logo"><img src="camping logo.png" alt="logo"></a>
+            <div class="navbar-right">
+              <a href="activiteiten.php">Activiteiten</a>
+              <a href="reserveren.php">Reserveren</a>
+              <a href="contact.php">Contact</a>
+              <a href="adminpage.php">Adminpage</a>
+            </div>
+          </nav>
+        </div>
+      </header>
+
+      <div class="body">
+        <h2>Activiteiten</h2>
+        <h4>Welkom op de site van Camping Village!</h4>
+      </div>
+    </div>
+  </div>
+</body>
+</html> 
---
+++ Beroepsproject camping/contact.php
@@ -0,0 +1,48 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  <title>Contact</title>
+</head>
+<body>
+<video autoplay muted loop id="myVideo">
+  <source src="production ID_4125025.mp4" type="video/mp4">
+</video>
+<div class="content">
+
+  <!-- Links -->
+  <header>
+  <div class="header">
+    <nav class="navigation">
+      <a href="index.php" class="navbar-logo"><img src="camping logo.png" alt="logo"></a>
+      <div class="navbar-right">
+        <a href="activiteiten.php">Activiteiten</a>
+        <a href="reserveren.php">Reserveren</a>
+        <a href="contact.php">Contact</a>
+        <a href="adminpage.php">Adminpage</a>
+      </div>
+    </nav>
+  </div>
+</header>
+
+  <div class="body">
+    <h2>Contact pagina</h2>
+    <h4>Adres: Zomerenstraat, 1234 AB <br>
+        E-mail: camping@village.nl <br> 
+        Telefoonnummer: 0612345678</h4>
+  </div>
+</div>
+</body>
+</html> 
